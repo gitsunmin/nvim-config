545eef3bc22b67bd50a430876f398aa4653037d4
김선민 <tjsals116@gmail.com>
2025-05-28 20:23:15 +0900

    add initial editor UI configuration

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- 에디터 UI 설정
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+
